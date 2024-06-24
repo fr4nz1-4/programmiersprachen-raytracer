64c5cea7972d2a982919814b49fe3bbc845ed04f
fr4nz1-4 <captain.proton.paris@gmail.com>
2024-06-24 15:55:14 +0200

    5.4: added Shape::print() and operator<<()

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,6 +1,16 @@
 #include "shape.hpp"
+#include "iostream"
 
-Shape::Shape(const std::string &name, const Color &color) :
+Shape::Shape(std::string const& name, Color const& color) :
     name_{name},
     color_{color}
 {}
+
+std::ostream& Shape::print(std::ostream &os) const {
+    std::string st = "name: " + name_ + " | Farbe: " + std::to_string(color_.r) + ", " + std::to_string(color_.g) + ", " + std::to_string(color_.b) ;
+    return os << st;
+}
+
+std::ostream& operator<<(std::ostream& os, Shape const& shape) {
+    return shape.print(os);
+}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -7,11 +7,14 @@
 class Shape {
 public:
     Shape(std::string const& name, Color const& color);
+    virtual std::ostream& print(std::ostream& os) const;
     virtual float area() const = 0;
     virtual float volume() const = 0;
 private:
     std::string name_;
     Color color_;
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& shape);
 
 #endif //RAYTRACER_SHAPE_HPP
