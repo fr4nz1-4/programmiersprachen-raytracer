9e8cb88929c628d82da898bf571ba7a6d43e1faf
fr4nz1-4 <captain.proton.paris@gmail.com>
2024-07-07 09:32:50 +0200

    add scene.hpp, scene.cpp and scene_1.sdf

---
+++ framework/scene.hpp
@@ -0,0 +1,18 @@
+#ifndef RAYTRACER_SCENE_HPP
+#define RAYTRACER_SCENE_HPP
+
+#include <glm/glm.hpp>
+
+#include <iostream>
+//file stream for opening files
+#include <fstream>
+#include <string>
+// stringstream for getting parts of the read string (tokens) and
+// streaming them into our Material Variables
+#include <sstream>
+#include <vector>
+
+struct Scene {
+
+};
+#endif //RAYTRACER_SCENE_HPP
