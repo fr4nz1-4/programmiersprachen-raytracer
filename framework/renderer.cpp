72fbd2e32d0649178c60a54899c97cc58084192a
fr4nz1-4 <captain.proton.paris@gmail.com>
2024-08-19 14:28:24 +0200

    add ambient light for spheres AND boxes

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -30,9 +30,9 @@ Material red = {"red", Color{1, 0, 0}, Color{1, 0, 0}, Color{1, 0, 0}, 20.0f};
 
 void Renderer::render() {
 
-    glm::vec3 camera_origin = {width_/2, height_/2, -1000.0f};  // Setze die Kamera weit vor die Szene
-//    glm::vec3 camera_origin = {-100.0f, 200.0f, -1000.0f};  // Setze die Kamera weit vor die Szene
-    glm::vec3 light_position = camera_origin;
+//    glm::vec3 camera_origin = {width_/2, height_/2, -200.0f};  // Setze die Kamera weit vor die Szene
+    glm::vec3 camera_origin = {0.0f, 0.0f, -400.0f};  // Setze die Kamera weit vor die Szene
+    glm::vec3 light_position = {300.0f, 200.0f , -200} ;
     Color light_intensity = {0.5f, 0.5f, 0.5f}; // Beispielintensität für das Licht
 
     for (unsigned y = 0; y < height_; ++y) {
@@ -65,12 +65,23 @@ void Renderer::render() {
                         // Ambienter Beleuchtungsanteil
                         Color ambient_component = {light_intensity.r * shape->get_Material()->ka.r, light_intensity.g * shape->get_Material()->ka.g, light_intensity.b * shape->get_Material()->ka.b};
                         glm::vec3 l = glm::normalize(light_position - hit.intersection_point); // Lichtvektor
+                        if (nullptr != std::dynamic_pointer_cast<Sphere>(shape)) {
+                            Sphere sphere = *std::dynamic_pointer_cast<Sphere>(shape);
+                            float diffuse_factor = std::max(glm::dot(glm::normalize(hit.intersection_point - sphere.get_center()), l), 0.0f); // Skalarprodukt von Normalenvektor und Lichtvektor
+                            Color tmp = {light_intensity.r * shape->get_Material()->kd.r, light_intensity.g * shape->get_Material()->kd.g, light_intensity.b * shape->get_Material()->kd.b};
+                            Color diffuse_component = {tmp.r * diffuse_factor, tmp.g * diffuse_factor, tmp.b * diffuse_factor};
+                            p.color = ambient_component + diffuse_component;
 
-                        Sphere sphere = *std::dynamic_pointer_cast<Sphere>(shape);
-                        float diffuse_factor = std::max(glm::dot(glm::normalize(hit.intersection_point - sphere.get_center()), l), 0.0f); // Skalarprodukt von Normalenvektor und Lichtvektor
-                        Color tmp = {light_intensity.r * shape->get_Material()->kd.r, light_intensity.g * shape->get_Material()->kd.g, light_intensity.b * shape->get_Material()->kd.b};
-                        Color diffuse_component = {tmp.r * diffuse_factor, tmp.g * diffuse_factor, tmp.b * diffuse_factor};
-                        p.color = ambient_component + diffuse_component;
+                        }
+                        else {
+                            std::cout << "Hit a box!" << std::endl;  //
+                            Box box = *std::dynamic_pointer_cast<Box>(shape);
+                            glm::vec3 normal = box.normal(hit.intersection_point);
+                            float diffuse_factor = std::max(glm::dot(normal, l), 0.0f);
+                            Color tmp = {light_intensity.r * shape->get_Material()->kd.r, light_intensity.g * shape->get_Material()->kd.g, light_intensity.b * shape->get_Material()->kd.b};
+                            Color diffuse_component = {tmp.r * diffuse_factor, tmp.g * diffuse_factor, tmp.b * diffuse_factor};
+                            p.color = ambient_component + diffuse_component;
+                        }
                     }
                 }
             }
